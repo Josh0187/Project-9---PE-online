a29729d402b977b678e44f8b7d8c9e327512a77b
Mistios <74119729+Mistios@users.noreply.github.com>
2020-11-09 03:26:51 +0500

    Add files via upload

--- app/src/main/java/com/example/peonline/MainActivity.java
+++ app/src/main/java/com/example/peonline/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.peonline;
 
+import android.content.Context;
 import android.content.Intent;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
 
+import com.example.peonline.gps.GPSTracker;
 import com.example.peonline.gps.LocationTracker;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
@@ -18,31 +22,48 @@ import android.view.View;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Toast;
 import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnShowLocation;
+
+    // GPSTracker class
+    GPSTracker gps;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnShowLocation = (Button) findViewById(R.id.btnShowLocation);
+
+        // Show location button click event
+        btnShowLocation.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                // Create class object
+                gps = new GPSTracker(MainActivity.this);
+
+                // Check if GPS enabled
+                if(gps.canGetLocation()) {
+
+                    double latitude = gps.getLatitude();
+                    double longitude = gps.getLongitude();
+
+                    // \n is for new line
+                    Toast.makeText(getApplicationContext(), "Your Location is - \nLat: " + latitude + "\nLong: " + longitude, Toast.LENGTH_LONG).show();
+                } else {
+                    // Can't get location.
+                    // GPS or network is not enabled.
+                    // Ask user to enable GPS/network in settings.
+                    gps.showSettingsAlert();
+                }
+            }
+        });
     }
-
-    @RequiresApi(api = Build.VERSION_CODES.P)
-    public void onToggleClicked(View view) {
-        // Is the toggle on?
-        boolean on = ((ToggleButton) view).isChecked();
-
-        if (on) {
-            Intent intent=new Intent("android.location.GPS_ENABLED_CHANGE");
-            intent.putExtra("enabled", true);
-            sendBroadcast(intent);
-
-        } else {
-            Intent intent = new Intent("android.location.GPS_ENABLED_CHANGE");
-            intent.putExtra("enabled", false);
-            sendBroadcast(intent);
-        }
-    }
-
 }
+
